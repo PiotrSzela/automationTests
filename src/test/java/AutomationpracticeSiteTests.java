85d9005d455f6a936931b83657571e26cfe6bceb
unknown <piotr.m.szela@gmail.com>
2022-07-06 19:59:51 +0200

    second version

--- src/test/java/AutomationpracticeSiteTests.java
+++ src/test/java/AutomationpracticeSiteTests.java
@@ -41,14 +41,14 @@ public class AutomationpracticeSiteTests {
         Assertions.assertEquals(1, elements.size());
         goToLogInSite();
         WebElement loginPageAuthentications = driver.findElement(By.className("page-heading"));
-        Assertions.assertEquals("AUTHENTICATION", loginPageAuthentications.getText());
+        Assertions.assertEquals("AUTHENTICATION", loginPageAuthentications.getText(),"I can't find Authentication");
         WebElement typePassword = driver.findElement(By.cssSelector("#passwd"));
         typePassword.click();
         typePassword.sendKeys("PeterGriffin1234");
         WebElement submitButton = driver.findElement(By.cssSelector("#SubmitLogin"));
         submitButton.click();
         WebElement errorMessage = driver.findElement(By.cssSelector(".alert-danger > ol "));
-        Assertions.assertEquals("An email address required.", errorMessage.getText());
+        Assertions.assertEquals("An email address required.", errorMessage.getText(),"There is no error message");
 
 
     }
@@ -65,7 +65,7 @@ public class AutomationpracticeSiteTests {
         WebElement submitButton = driver.findElement(By.cssSelector("#SubmitLogin"));
         submitButton.click();
         WebElement errorMessage = driver.findElement(By.cssSelector(".alert-danger > ol "));
-        Assertions.assertEquals("Password is required.", errorMessage.getText());
+        Assertions.assertEquals("Password is required.", errorMessage.getText(),"There is no error message");
     }
 
     // Test 3
@@ -99,7 +99,7 @@ public class AutomationpracticeSiteTests {
         WebElement gotoContactUsSite = driver.findElement(By.cssSelector("#contact-link"));
         gotoContactUsSite.click();
         WebElement checkContactUs = driver.findElement(By.className("navigation_page"));
-        Assertions.assertEquals("Contact", checkContactUs.getText());
+        Assertions.assertEquals("Contact", checkContactUs.getText(),"There is no any Contact");
 
     }
 
@@ -112,7 +112,7 @@ public class AutomationpracticeSiteTests {
         WebElement gotoMainSite = driver.findElement(By.cssSelector("#header_logo > a"));
         gotoMainSite.click();
         String checkMainSiteUrl = driver.getCurrentUrl();
-        Assertions.assertEquals("http://automationpractice.com/index.php", checkMainSiteUrl);
+        Assertions.assertEquals("http://automationpractice.com/index.php", checkMainSiteUrl,"wrong URL");
     }
     // Test 6  -------------------------------------------------
     @Test
@@ -123,28 +123,31 @@ public class AutomationpracticeSiteTests {
         WebElement addProduct = driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[1]/div/div[2]/div[2]/a[1]/span"));
         moveMouse.click(addProduct);
         WebElement isAddToCart = driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/h2/span[2]"));
-        Assertions.assertEquals("There is 1 item in your cart.",isAddToCart.getText());
+        Assertions.assertEquals("There is 1 item in your cart.",isAddToCart.getText(),"don't find item");
 
     }
 
     // Test 7  -------------------------------------------------
     @Test
     void checkDeleteProductFromCart() {
-        WebElement product = driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[1]"));
-        product.click();
-        WebElement addButton = driver.findElement(By.id("add_to_cart"));
-        addButton.click();
-        WebElement isAddToCart = driver.findElement(By.className("icon-ok"));
-        Assertions.assertTrue(isAddToCart.isDisplayed());
-        WebElement closeButton = driver.findElement(By.className(".cross"));
+        WebElement searchItemToAdd = driver.findElement(By.id("search_query_top"));
+        searchItemToAdd.click();
+        searchItemToAdd.sendKeys("Blouse");
+        WebElement findItem = driver.findElement(By.className("button-search"));
+        findItem.click();
+        WebElement changeToList = driver.findElement(By.className("icon-th-list"));
+        changeToList.click();
+        WebElement addToCartButton = driver.findElement(By.cssSelector("a.button.ajax_add_to_cart_button.btn.btn-default > span"));
+        addToCartButton.click();
+        WebElement closeButton = driver.findElement(By.cssSelector(".cross"));
         closeButton.click();
         Actions moveMouse = new Actions(driver);
+        Wait wait = new WebDriverWait(driver, Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("span.ajax_cart_product_txt.unvisible")));
         moveMouse.moveToElement(driver.findElement(By.cssSelector(".ajax_cart_product_txt.unvisible")));
-        Wait wait = new WebDriverWait(driver, Duration.ofSeconds(1));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("cart-info")));
         moveMouse.click(driver.findElement(By.className("ajax_cart_block_remove_link")));
-        WebElement isCartEmpty = driver.findElement(By.className("ajax_cart_no_product"));
-        Assertions.assertEquals("(empty)",isCartEmpty.getText());
+        WebElement isCartEmpty = driver.findElement(By.cssSelector("span.ajax_cart_quantity.unvisible"));
+        Assertions.assertTrue(isCartEmpty.isEnabled());
 
     }
 
@@ -153,7 +156,7 @@ public class AutomationpracticeSiteTests {
     void logInTest() {
         goToLogInSite();
         login("test@softie.pl","1qaz!QAZ");
-        Assertions.assertEquals("My account",driver.findElement(By.className("navigation_page")).getText());
+        Assertions.assertEquals("My account",driver.findElement(By.className("navigation_page")).getText(),"log in is impossible");
     }
 
     // Functions -----------------------------------------------
@@ -161,10 +164,10 @@ public class AutomationpracticeSiteTests {
     // login ------------------------------------------------
     public void login(String email,String psswrd){
         WebElement findPasswordInput = driver.findElement(By.id("passwd"));
-        findPasswordInput.sendKeys(psswrd);
         WebElement findLoginInput = driver.findElement(By.id("email"));
-        findLoginInput.sendKeys(email);
         WebElement signIn = driver.findElement(By.id("SubmitLogin"));
+        findPasswordInput.sendKeys(psswrd);
+        findLoginInput.sendKeys(email);
         signIn.click();
 
     }
